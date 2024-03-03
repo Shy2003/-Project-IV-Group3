52cbbe1667ae630c4aeae293d1f660a5fbc10117
brianegona <begona4038@conestogac.on.ca>
2024-03-03 15:42:39 -0500

    GUI Implemented

--- Shopping App/Server/Server/ServerChatBox.Designer.cs
+++ Shopping App/Server/Server/ServerChatBox.Designer.cs
@@ -28,18 +28,98 @@
         /// </summary>
         private void InitializeComponent()
         {
+            label1 = new Label();
+            label2 = new Label();
+            ServerRunningLabel = new Label();
+            SendButton = new Button();
+            BroadcastMessageTextBox = new TextBox();
+            MessageDisplayListBox = new ListBox();
             SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(294, 23);
+            label1.Name = "label1";
+            label1.Size = new Size(140, 20);
+            label1.TabIndex = 12;
+            label1.Text = "Received Messages:";
+            label1.Click += label1_Click;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(294, 420);
+            label2.Name = "label2";
+            label2.Size = new Size(205, 20);
+            label2.TabIndex = 11;
+            label2.Text = "Enter a Message to Broadcast";
+            label2.Click += label2_Click;
+            // 
+            // ServerRunningLabel
+            // 
+            ServerRunningLabel.AutoSize = true;
+            ServerRunningLabel.Location = new Point(12, 648);
+            ServerRunningLabel.Name = "ServerRunningLabel";
+            ServerRunningLabel.Size = new Size(108, 20);
+            ServerRunningLabel.TabIndex = 10;
+            ServerRunningLabel.Text = "Server Running";
+            ServerRunningLabel.Click += ServerRunningLabel_Click;
+            // 
+            // SendButton
+            // 
+            SendButton.Location = new Point(294, 476);
+            SendButton.Name = "SendButton";
+            SendButton.Size = new Size(80, 29);
+            SendButton.TabIndex = 9;
+            SendButton.Text = "Send";
+            SendButton.UseVisualStyleBackColor = true;
+            SendButton.Click += SendButton_Click;
+            // 
+            // BroadcastMessageTextBox
+            // 
+            BroadcastMessageTextBox.Location = new Point(294, 443);
+            BroadcastMessageTextBox.Name = "BroadcastMessageTextBox";
+            BroadcastMessageTextBox.Size = new Size(316, 27);
+            BroadcastMessageTextBox.TabIndex = 8;
+            BroadcastMessageTextBox.TextChanged += BroadcastMessageTextBox_TextChanged;
+            // 
+            // MessageDisplayListBox
+            // 
+            MessageDisplayListBox.FormattingEnabled = true;
+            MessageDisplayListBox.ItemHeight = 20;
+            MessageDisplayListBox.Location = new Point(294, 58);
+            MessageDisplayListBox.Name = "MessageDisplayListBox";
+            MessageDisplayListBox.Size = new Size(651, 344);
+            MessageDisplayListBox.TabIndex = 7;
+            MessageDisplayListBox.SelectedIndexChanged += MessageDisplayListBox_SelectedIndexChanged;
             // 
             // ServerChatBox
             // 
-            AutoScaleDimensions = new SizeF(12F, 30F);
+            AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(1896, 1016);
+            ClientSize = new Size(1264, 677);
+            Controls.Add(label1);
+            Controls.Add(label2);
+            Controls.Add(ServerRunningLabel);
+            Controls.Add(SendButton);
+            Controls.Add(BroadcastMessageTextBox);
+            Controls.Add(MessageDisplayListBox);
+            Margin = new Padding(2);
             Name = "ServerChatBox";
             Text = "ServerChatBox";
+            Load += ServerChatBox_Load;
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label label1;
+        private Label label2;
+        private Label ServerRunningLabel;
+        private Button SendButton;
+        private TextBox BroadcastMessageTextBox;
+        private ListBox MessageDisplayListBox;
     }
 }
--- Shopping App/Server/Server/ServerChatBox.cs
+++ Shopping App/Server/Server/ServerChatBox.cs
@@ -4,6 +4,8 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Net.Sockets;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -12,9 +14,152 @@ namespace Server
 {
     public partial class ServerChatBox : Form
     {
+        private TcpListener tcpListener;
+        private List<TcpClient> connectedClients = new List<TcpClient>();
+        private Thread listenThread;
+
         public ServerChatBox()
         {
             InitializeComponent();
+            StartServer();
+        }
+
+        private void StartServer()
+        {
+            tcpListener = new TcpListener(IPAddress.Any, 8080);
+            listenThread = new Thread(new ThreadStart(ListenForClients));
+            listenThread.Start();
+        }
+
+        private void ListenForClients()
+        {
+            tcpListener.Start();
+
+            while (true)
+            {
+                TcpClient client = tcpListener.AcceptTcpClient();
+                connectedClients.Add(client);
+
+                Thread clientThread = new Thread(new ParameterizedThreadStart(HandleClientComm));
+                clientThread.Start(client);
+            }
+        }
+
+
+        private void HandleClientComm(object client)
+        {
+            TcpClient tcpClient = (TcpClient)client;
+            NetworkStream clientStream = tcpClient.GetStream();
+
+            byte[] messageBuffer = new byte[4096];
+            StringBuilder completeMessage = new StringBuilder();
+            int bytesRead;
+
+            while (true)
+            {
+                try
+                {
+                    bytesRead = clientStream.Read(messageBuffer, 0, messageBuffer.Length);
+                }
+                catch (Exception ex)
+                {
+                    // Handle exception (e.g., client disconnected)
+                    Console.WriteLine("Error reading message: " + ex.Message);
+                    break;
+                }
+
+                if (bytesRead == 0)
+                {
+                    // Client disconnected
+                    break;
+                }
+
+                string message = Encoding.ASCII.GetString(messageBuffer, 0, bytesRead);
+                completeMessage.Append(message);
+
+                // Check for end of message
+                if (completeMessage.ToString().Contains("<EOF>"))
+                {
+                    string finalMessage = completeMessage.ToString().Replace("<EOF>", "");
+
+                    // Display message in the ListBox
+                    AddMessageToListBox(finalMessage);
+
+                    completeMessage.Clear();
+                }
+            }
+
+            // Remove client from the list of connected clients
+            connectedClients.Remove(tcpClient);
+        }
+
+        private void AddMessageToListBox(string message)
+        {
+            if (MessageDisplayListBox.InvokeRequired)
+            {
+                MessageDisplayListBox.Invoke(new MethodInvoker(delegate
+                {
+                    MessageDisplayListBox.Items.Add(message);
+                }));
+            }
+            else
+            {
+                MessageDisplayListBox.Items.Add(message);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // This is the event handler for the "Send" button
+            string message = BroadcastMessageTextBox.Text;
+
+            // Broadcast the message to all connected clients
+            byte[] messageBytes = Encoding.ASCII.GetBytes(message + "<EOF>");
+            foreach (TcpClient client in connectedClients)
+            {
+                NetworkStream stream = client.GetStream();
+                stream.Write(messageBytes, 0, messageBytes.Length);
+            }
+
+            // Clear the broadcast message TextBox
+            BroadcastMessageTextBox.Clear();
+        }
+
+
+
+        private void ServerChatBox_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void SendButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void MessageDisplayListBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BroadcastMessageTextBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ServerRunningLabel_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
